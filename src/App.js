6fd308a9248d2dde28ac7fa1e4b9c75da9e5d274
srinumeesala <srinusri76585@gmail.com>
2023-03-04 14:30:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import ProductItemDetails from './components/ProductItemDetails';
+import Products from './components/Products';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Products} />
+      <Route exact path="/products/:id" component={ProductItemDetails} />
+    </Switch>
+  </BrowserRouter>
+)
 
 export default App;
---
+++ src/components/Products/index.js
@@ -0,0 +1,107 @@
+import { useEffect, useState } from "react"
+import ProductItem from "../ProductItem"
+import { v4 as uuidv4 } from "uuid"
+import Loader from 'react-loader-spinner'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+import "./index.css"
+
+const url = "http://localhost:4000/products"
+const apiStatusConstants = {
+    fail: "Failed",
+    success: "Successful",
+    load: "Loading",
+    initial: 'inital'
+}
+const Products = () => {
+    const [products, setProducts] = useState([{}])
+    const [productsAPIStaus, setProductsAPIStatus] = useState(apiStatusConstants.initial)
+    console.log(products)
+
+    const getProducts = async () => {
+        setProductsAPIStatus(apiStatusConstants.load)
+        const options = {
+            method: "GET",
+            headers: {
+                "content-type": "application/json"
+            }
+        }
+        try {
+            const response = await fetch(url, options)
+            const result = await response.json()
+            setProducts(result.productsList)
+            setProductsAPIStatus(apiStatusConstants.success)
+        } catch (error) {
+            console.log("Something went wrong in products api call", error)
+            setProductsAPIStatus(apiStatusConstants.fail)
+        }
+    }
+
+    useEffect(() => {
+        getProducts()
+    }, [])
+
+    const renderSuccessView = () => (
+        <div className="productsParentCon d-flex justify-content-center">
+            <div className="productsCon mt-3">
+                <h1 className="h2 mb-3">Products</h1>
+                <ul className="list-unstyled d-flex row" >
+                    {
+                        products.map(obj => <ProductItem key={uuidv4()} eachItem={obj} />)
+                    }
+                </ul>
+            </div>
+        </div>
+    )
+
+
+    const renderFailureView = () => (
+        <div className="failview min-vh-100 d-flex flex-column justify-content-center align-items-center">
+            <div>
+                <div className="text-center">
+                    <img
+                        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-error-view-img.png"
+                        alt="products failure"
+                        className="sizeFailure"
+                    />
+                </div>
+                <h1 className="text-center">Something Went Wrong.</h1>
+                <div className="text-center">
+                    <button
+                        type="button"
+                        className="btn btn-primary"
+                    >
+                        Try Again
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+
+    const renderLoadingView = () => (
+        <div
+            className="text-center loader d-flex justify-content-center align-items-center vh-100"
+            testid="loader"
+        >
+            <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+        </div>
+    )
+
+
+
+    const renderUi = () => {
+        switch (productsAPIStaus) {
+            case apiStatusConstants.success:
+                return renderSuccessView()
+            case apiStatusConstants.fail:
+                return renderFailureView()
+            case apiStatusConstants.load:
+                return renderLoadingView()
+            default:
+                return null
+        }
+    }
+
+    return <>{renderUi()}</>
+}
+
+export default Products
